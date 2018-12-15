be3c32d100763a8bd5a6c7a2fa67484c9202e3a2
Lina Adkins <6924569+SwapBap@users.noreply.github.com>
2018-12-15 15:54:14 -0500

    Add push button screen change controls

--- WemosDustSensor.ino
+++ WemosDustSensor.ino
@@ -9,6 +9,13 @@ PMS::DATA data;
 Adafruit_SSD1306 OLED(OLED_RESET);
 #define MODE_TOTALS 1
 #define MODE_GRAMS 2
+#define MODE_GRAMS_CF0 3
+
+const int togglePin = D7;     // the number of the pushbutton pin
+int toggleState = 1;          // Pin is pulled up high by default
+int lastToggleState = 1;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 500;
 
 byte dispMode = MODE_TOTALS;
  
@@ -16,7 +23,7 @@ void setup()   {
   OLED.begin();
   OLED.clearDisplay();
  
-  //Add stuff into the 'display buffer'
+  // Set up screen
   OLED.setTextWrap(false);
   OLED.setTextSize(1);
   OLED.setTextColor(WHITE);
@@ -26,15 +33,49 @@ void setup()   {
   OLED.display(); //output 'display buffer' to screen  
 
   Serial.begin(9600);   // GPIO1, GPIO3 (TX/RX pin on ESP-12E Development Board)
+
+  pinMode( togglePin , INPUT_PULLUP ); // Pin for switching screens button
+
 } 
+
+// Handle reading the display change pin and switching the display mode
+void DisplayToggle(){
+
+  lastToggleState = toggleState;
+  toggleState = digitalRead(togglePin);
+
+  // If button is pressed and it is different from last state.
+  if(toggleState == LOW && toggleState != lastToggleState ){
+
+      // We haven't waited long enough so consider press illegitimate
+      if(millis() - lastDebounceTime <= debounceDelay){
+        return;
+      }
+
+      // Switch display
+      lastDebounceTime = millis();
+      if(dispMode >= 3){
+        dispMode = 1;
+        return;
+      }
+      else{
+        dispMode++;
+        return;
+      }
+   }
+}
+
  
 void loop() {
+
+  DisplayToggle();
   
   if (pms.read(data))
   {
     OLED.clearDisplay();
     OLED.setCursor(0,0);
 
+    // Render our displays
     switch(dispMode){
       case MODE_TOTALS:
           OLED.println("Total particles");
@@ -51,17 +92,23 @@ void loop() {
           OLED.print("5M: "); OLED.print(data.PM_TOTALPARTICLES_5_0);  
           OLED.print(" 10M: "); OLED.println(data.PM_TOTALPARTICLES_10_0);
 
-
-          //OLED.print("PM 0.5 : ");
-          //OLED.println(data.PM_TOTALPARTICLES_0_5);
-
-          //OLED.print("PM 1.0 : ");
-          //OLED.println(data.PM_TOTALPARTICLES_1_0);
-
           break;
           
       case MODE_GRAMS:
-          OLED.println("Micrograms per Meter^3");
+          OLED.println("ug/m^3 (Atmos.)");
+          OLED.print("PM 1.0 (ug/m3): ");
+          OLED.println(data.PM_AE_UG_1_0);
+
+          OLED.print("PM 2.5 (ug/m3): ");
+          OLED.println(data.PM_AE_UG_2_5);
+
+          OLED.print("PM 10.0 (ug/m3): ");
+          OLED.println(data.PM_AE_UG_10_0);
+
+
+          break;
+      case MODE_GRAMS_CF0:
+          OLED.println("ug/m^3 (CF=1)");
           OLED.print("PM 1.0 (ug/m3): ");
           OLED.println(data.PM_AE_UG_1_0);
 
@@ -73,13 +120,17 @@ void loop() {
 
           break;
       
+      default:
+          OLED.println(dispMode);
+          break;
+      
     }
 
 
     OLED.display();
     delay(0.25);
 
-  }
+    
 
-  // Do other stuff...
+  }
 }
